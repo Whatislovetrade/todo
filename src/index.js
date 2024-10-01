2996085b12a0faf0bb584400bb784498a23f9038
Vlad Ivannikov <vladmaslow1@gmail.com>
2024-10-01 16:42:05 +0500

    base react elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hellow World')
+import React from "react";
+import ReactDOM from "react-dom/client"; // В React 18 используем 'react-dom/client'
+
+const el = (
+  <div>
+      <h1>My Todo List</h1>
+      <input placeholder="search"></input>
+      <ul>
+        <li>Learn React</li>
+        <li>Build Awesome App</li>
+      </ul>
+  </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(el);
+

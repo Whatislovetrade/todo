ab6ac3d7a1effc51faa4453c46e308dd43c3ba99
Vlad Ivannikov <vladmaslow1@gmail.com>
2024-11-01 14:03:22 +0500

    add css in react and use bootstrap in todo

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // В React 18 используем 'react-dom/client'
 import AppHeader from "./components/app-header";
-import TodoList from "./components/todo-list";
 import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
+import ItemStatusFilter from './components/item-status-filter';
+
+import './index.css'
 
 const App = () => {
 
   const todoData = [
-    {label: 'Drink Coffee', important: false},
-    {label: 'Make Awesome App', important: true},
-    {label: 'Have a lunch', important: false},
+    {label: 'Drink Coffee', important: false, id: 1},
+    {label: 'Make Awesome App', important: true, id: 2},
+    {label: 'Have a lunch', important: false, id: 3},
   ]
-
   return (
-    <div>
-    <AppHeader />
-    <SearchPanel />
-    <TodoList todos = {todoData}/>
-  </div>
-  )
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
+    </div>
+  );
   
 }
 

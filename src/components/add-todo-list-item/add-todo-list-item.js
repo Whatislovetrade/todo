cffb9b07f6ff350308c2de54b4c2264c6ea85bfa
Vlad <vladmaslow1@gmail.com>
2024-12-13 19:38:59 +0500

    add state in add-todo-list-item and use onChange

--- src/components/add-todo-list-item/add-todo-list-item.js
+++ src/components/add-todo-list-item/add-todo-list-item.js
@@ -2,16 +2,36 @@ import { Component } from 'react'
 import './add-todo-list-item.css'
 
 export default class AddTodoListItem extends Component {
+
+    state = {
+        label: ''
+    }
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        this.props.addItem(this.state.label)
+    }
+
     render() {
         return (
-            <div className="add-todo-list-item d-flex">
-                <input type="text" className="add-item search-input form-control" />
+            <form className="add-todo-list-item d-flex"
+                  onSubmit={this.onSubmit}>
+                <input type="text"
+                className="add-item search-input form-control" 
+                onChange={this.onLabelChange}
+                placeholder='Добавить текст'
+                />
                 <button 
-                    onClick={() => this.props.addItem('Hellow World') } 
                     className="btn btn-info"
                     >Add
                 </button>
-            </div>
+            </form>
         )
     }
 }

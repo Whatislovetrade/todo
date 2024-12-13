4b5611b009b756690950755e2ed1f8bd0abfe10e
Vlad <vladmaslow1@gmail.com>
2024-12-13 19:17:36 +0500

    moved data in app

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -2,7 +2,7 @@ import React from "react";
 import TodoListItem from '../todo-list-item'
 import './todo-list.css'
 
-const TodoList = ({todos, onDeleted, addItem}) => {
+const TodoList = ({todos, onDeleted, onToggleImportant, onToggleDone}) => {
 
   const elements = todos.map((item) => {
 
@@ -13,6 +13,8 @@ const TodoList = ({todos, onDeleted, addItem}) => {
         <TodoListItem 
             {...itemProps }
             onDeleted={() => onDeleted(id)}
+            onToggleImportant ={() => onToggleImportant(id)}
+            onToggleDone ={() => onToggleDone(id)}
           />
       </li>
     )
